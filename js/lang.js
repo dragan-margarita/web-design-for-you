1f8a0f563c79a57f14ea0dd2bdbab45165aabbfd
dragan-margarita <61410866+dragan-margarita@users.noreply.github.com>
2023-03-15 10:50:44 +0200

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,322 @@
+const langArr = {
+		"unit":{
+			"en":"Website development for your business | To order a site",
+			"ua":"Розробка сайтів для Вашого бізнесу | Замовити сайт",
+			"ru":"Разработка сайтов для Вашего бизнеса | Заказать сайт",
+		},
+		"mi-about_us":{
+			"en":"Aboute us",
+			"ua":"Про нас",
+			"ru":"О нас",
+		},
+		"mi-portfolio":{
+			"en":"Projects",
+			"ua":"Проекти",
+			"ru":"Проекты",
+		},
+		"mi-services":{
+			"en":"Services",
+			"ua":"Послуги",
+			"ru":"Услуги",
+		},
+		"mi-work":{
+			"en":"Workflow",
+			"ua":"Процес роботи",
+			"ru":"Процесс работы",
+		},
+		"mi-contacts":{
+			"en":"Contacts",
+			"ua":"Контакти",
+			"ru":"Контакты",
+		},
+		"telephone":{
+			"en":"Telephone",
+			"ua":"Телефон",
+			"ru":"Телефон",
+		},
+		"socicons":{
+			"en":"We are on social networks",
+			"ua":"Ми у соцмережах",
+			"ru":"Мы в соцсетях",
+		},
+		"txtgeneral":{
+			"en":"Website development",
+			"ua":"Розробка сайтів",
+			"ru":"Разработка сайтов",
+		},
+		"txtgeneral-1":{
+			"en":"We develop websites for your business at the best price for you!",
+			"ua":"Ми розробляємо сайти для Вашого бізнесу за найвигіднішою для Вас ціною!",
+			"ru":"Мы разрабатываем сайты для Вашего бизнеса по самой выгодной для Вас цене!",
+		},
+		"txtgeneral-2":{
+			"en":"Modern technologies, trendy design, corporate identity and promotional products - you can order all this right now!!!",
+			"ua":"Сучасні технології, трендовий дизайн, фірмовий стиль та рекламна продукція – все це Ви можете замовити вже зараз!",
+			"ru":"Современные технологии, трендовый дизайн, фирменный стиль и рекламная продукция - всё это Вы можете заказать уже сейчас!!!",
+		},
+		"button-g":{
+			"en":"To order a site",
+			"ua":"Замовити сайт",
+			"ru":"Заказать сайт",
+		},
+		"aboute-us":{
+			"en":"Why should you order a website from us?",
+			"ua":"Чому Вам варто замовити сайт?",
+			"ru":"Почему Вам стоит заказать сайт у нас?",
+		},
+		"text-aboute-us":{
+			"en":"In our time, the site is a ''must have'' for any kind of activity, it is the main means of information. If your business is not on the Internet, then you do not have a business!<br>Our goal is to increase your profit by expanding the circle of your customers, customers and customers. To do this, together we will create the concept of your site based on an effective and efficient design and user-friendly interface.",
+			"ua":"В наш час сайт – це «must have» для будь-якої діяльності, це основний засіб інформації. Якщо Вашого бізнесу немає в інтернеті, то у Вас немає бізнесу! Наша мета - збільшити Ваш прибуток, розширивши коло Ваших покупців, клієнтів та замовників. Для цього ми разом створимо концепцію Вашого сайту на основі ефектного та ефективного дизайну та зручного інтерфейсу користувача.",
+			"ru":"В наше время сайт - это  «must have» для любого рода деятельности, это основное средство информации. Если Вашего бизнеса нет в интернете, то у Вас нет бизнеса!<br>Наша цель - увеличить Вашу прибыль, расширив круг Ваших покупателей, клиентов и заказчиков. Для этого мы вместе создадим концепцию Вашего сайта на основе эффектного и эффективного дизайна и удобного интерфейса пользователя.",
+		},
+		"card-1":{
+			"en":"Modern design",
+			"ua":"Сучасний дизайн",
+			"ru":"Современный дизайн",
+		},
+		"card-1-text":{
+			"en":"Each of our projects is unique and inimitable! Particular attention is paid to the development of website design. Design is not luxury, but convenience. And first of all, it is the convenience of use for site visitors. We have been working in the field of design for more than 10 years.<br> We work for the result, and our result is satisfied customers!",
+			"ua":"Кожен наш проект унікальний та неповторний! Особлива увага приділяється розробці дизайну сайту. Дизайн – це не розкіш, а зручність. І насамперед це зручність користування для відвідувачів сайту. Ми більше 10 років працюємо у сфері дизайну.<br> Ми працюємо на результат, а наш результат – задоволені клієнти!",
+			"ru":"Каждый наш проект уникальный и неповторимый! Особое внимание уделяется разработке дизайна сайта. Дизайн - это не роскошь, а удобство. И прежде всего это удобство пользования для посетителей сайта. Мы больше 10 лет работаем в сфере дизайна.<br> Мы работаем на результат, а наш результат - довольные клиенты!",
+		},
+		"card-2":{
+			"en":"Adaptive layout",
+			"ua":"Адаптивна верстка",
+			"ru":"Адаптивная вёрстка",
+		},
+		"card-2-text":{
+			"en":"Quality is one of the main principles of our work.<br> The most striking indicator of quality is the adaptability of your site! What is this? This is an opportunity to view the site on different devices.<br>Absolutely all visitors to your site will easily find the necessary information!",
+			"ua":"Якість – один із головних принципів нашої роботи. <br> Найяскравішим показником якості є адаптивність Вашого сайту! Що це? Це можливість переглядати сайт на різних утройствах. Абсолютно всі відвідувачі Вашого сайту з легкістю знайдуть необхідну інформацію!",
+			"ru":"Качество – один из главных принципов нашей работы.<br> Самым ярким показателем качества является адаптивность Вашего сайта! Что это? Это возможность просматривать сайт на разных утройствах.<br>Абсолютно все посетители Вашего сайта с лёгкостью найдут необходимую информацию!",
+		},
+		"card-3":{
+			"en":"Nice rates",
+			"ua":"Приємні ціни",
+			"ru":"Приятные расценки",
+		},
+		"card-3-text":{
+			"en":"We value our clients! Our work gives us pleasure, so we can boast of the best value for money!!!<br> We will help to convey information about you to the target audience! Would you like to talk about a product, service or company? Call now!",
+			"ua":"Ми цінуємо своїх клієнтів! Наша робота приносить нам задоволення, тому ми можемо похвалитися оптимальним співвідношенням ціни та якості!!!<br> Ми допоможемо донести інформацію про Вас до цільової аудиторії! Бажаєте розповісти про товар, послугу чи компанію??? Дзвоніть прямо зараз!",
+			"ru":"Мы ценим своих клиентов! Наша работа доставляет нам удовольствие, поэтому мы можем похвастаться оптимальным соотношением цены и качества!!!<br> Мы поможем донести информацию о Вас до целевой аудитории! Хотите рассказать о товаре,услуге или компании??? Звоните прямо сейчас!",
+		},		
+		"portfolio":{
+			"en":"Our projects",
+			"ua":"Наші проекти",
+			"ru":"Наши проекты",
+		},
+		"text-portfolio":{
+			"en":"Our work is our pride!",
+			"ua":"Роботи, виконані нами – наша гордість!",
+			"ru":"Работы, выполненные нами - наша гордость!",
+		},
+		"project-1":{
+			"en":"Website-business card of the state kindergarten",
+			"ua":"Сайт-візитка державного дитячого садка",
+			"ru":"Сайт-визитка государственного детского сада",
+		},
+		"project-2":{
+			"en":"Site-card of the cafe 'Castle'",
+			"ua":"Сайт-візитка кафе 'Замок'",
+			"ru":"Сайт-визитка кафе 'Замок'",
+		},
+		"project-3":{
+			"en":"Graphic designer portfolio site",
+			"ua":"Сайт-портфоліо графічного дизайнера",
+			"ru":"Сайт-портфолио графического дизайнера",
+		},
+		"project-4":{
+			"en":"International recruiting company website",
+			"ua":"Сайт міжнародної рекрутингової компанії",
+			"ru":"Сайт международной рекрутинговой компании",
+		},		
+		"price":{
+			"en":"Cost of services",
+			"ua":"Вартість послуг",
+			"ru":"Стоимость услуг",
+		},
+		"box-1":{
+			"en":"Branding",
+			"ua":"Брендінг",
+			"ru":"Брендинг",
+		},
+		"box-1-text":{
+			"en":"Branding is first of all creation of image of your company. How will your customers, visitors or buyers remember it???<br> Logo and corporate identity development, motion design elements, promotional products with an integrated approach are designed to visualize the company in the eyes of the target audience and gain favor in a few seconds. If you want to be trusted, you should be known, you should be recognized!!!",
+			"ua":"Брендінг – це насамперед створення іміджу Вашої компанії. Який її запам'ятають Ваші клієнти, відвідувачі чи покупці???<br>Розробка логотипу та фірмового стилю, елементи моушн-дизайну, рекламна продукція при комплексному підході покликані візуалізувати компанію в очах цільової аудиторії та за кілька секунд завоювати прихильність. Якщо Ви хочете, щоб Вам довіряли, про Вас повинні знати, Вас повинні дізнаватися!",
+			"ru":"Брендинг - это прежде всего создание имиджа Вашей компании. Какой её запомнят Ваши клиенты, посетители или покупатели???<br>Разработка логотипа и фирменного стиля, элементы моушн-дизайна, рекламная продукция при комплексном подходе призваны визуализировать компанию в глазах целевой аудитории и за несколько секунд завоевать расположение. Если Вы хотите, чтоб Вам доверяли, о Вас должны знать, Вас должны узнавать!!!",
+		},
+		"price-1":{
+			"en":"from 500 &#8372;",
+			"ua":"від 500 &#8372;",
+			"ru":"от 500 &#8372;",
+		},
+		"box-2-text":{
+			"en":"Landing pages are an integral part of any online marketing campaign, and the popularity of this tool is growing every year. They are designed in such a way as to be able to succinctly but succinctly explain to a potential buyer the advantages of your company and kindle his interest so much that he takes the action you need without much thought.",
+			"ua":"Лендінг є невід'ємною частиною будь-якої маркетингової кампанії в мережі, і популярність цього інструменту зростає з року в рік. Вони проектуються таким чином, щоб зуміти лаконічно, але ємно пояснити потенційному покупцеві переваги Вашої компанії і так розпалити в ньому інтерес, щоб він зробив потрібну Вам дію без зайвих роздумів.",
+			"ru":"Лендинги являются неотъемлемой частью любой маркетинговой кампании в сети, и популярность этого инструмента растет из года в год. Они проектируются таким образом, чтобы суметь лаконично, но емко объяснить потенциальному покупателю преимущества Вашей компании и так сильно разжечь в нем интерес, чтобы он совершил нужное Вам действие без лишних раздумий."
+		},
+		"price-2":{
+			"en":"from 4000 &#8372;",
+			"ua":"від 4000 &#8372;",
+			"ru":"от 4000 &#8372;",
+		},
+		"box-3":{
+			"en":"Business card site",
+			"ua":"Сайт-візитівка",
+			"ru":"Сайт-визитка",
+		},
+		"box-3-text":{
+			"en":"In fact, a business card site is your business card on the Internet. Its main purpose is to briefly convey information about a company or person to attract the attention of visitors.<br>The main element of a business card site is the main page, which should contain all the necessary information about the company or person.",
+			"ua":"По суті сайт-візитка - це Ваша візитна картка на просторах Інтернету. Її основна мета - коротка передача інформації про компанію або персону для привернення уваги відвідувачів.<br>Основний елемент сайту-візитки - це головна сторінка, яка повинна містити всю необхідну інформацію про компанію або людину.",
+			"ru":"По сути, сайт-визитка - это Ваша визитная карточка на просторах Интернета. Ее основная цель краткая передача информации о компании или персоне для привлечения внимания посетителей.<br>Основный элемент сайта-визитки - это главная страница, которая должна содержать всю необходимую информацию о компании или человеке.",
+		},
+		"price-3":{
+			"en":"from 5000 &#8372;",
+			"ua":"від 5000 &#8372;",
+			"ru":"от 5000 &#8372;",
+		},
+		"factors":{
+			"en":"What determines the cost of website development?",
+			"ua":"Від чого залежить вартість розробки?",
+			"ru":"От чего зависит стоимость разработки сайта?",
+		},
+		"factor-1":{
+			"en":"Site type;",
+			"ua":"Тип сайту;",
+			"ru":"Тип сайта;",
+		},
+		"factor-2":{
+			"en":"The complexity of the technical task;",
+			"ua":"Складність технічного завдання;",
+			"ru":"Сложность технического задания;",
+		},
+		"factor-3":{
+			"en":"Number of functional developments to standard functionality;",
+			"ua":"Кількість функціональних розробок до стандартного функціоналу;",
+			"ru":"Количество функциональных разработок к стандартному функционалу;",
+		},
+		"factor-4":{
+			"en":"The need to involve third-party specialists (photographers, videographers, etc.)",
+			"ua":"Необхідність залучення сторонніх фахівців (фотографів, відеооператорів та ін.)",
+			"ru":"Необходимость привлечения сторонних специалистов (фотографов, видеооператоров и др.)",
+		},
+		"factor-5":{
+			"en":"Writing texts and filling the site with content.",
+			"ua":"Написання текстів та наповнення сайту контентом.",
+			"ru":"Написание текстов и наполнение сайта контентом.",
+		},
+		"work-title":{
+			"en":"How we are working:",
+			"ua":"Як ми працюємо:",
+			"ru":"Как мы работаем:",
+		},		
+		"ah-1":{
+			"en":"You contact us",
+			"ua":"Ви зв'язуєтесь з нами",
+			"ru":"Вы связываетесь с нами",
+		},
+		"at-1":{
+			"en":"After we discuss the project and find a suitable solution, we think over the idea of ​​the resource. After the tasks are set, the target audience is determined. We create a site structure to reflect all targeted requests. We form a technical task.",
+			"ua":"Після цього ми обговорюємо проект і знаходимо відповідне рішення, продумуємо ідею ресурсу. Після того, як поставлено завдання, визначається цільова аудиторія. Створюємо структуру сайту для відображення всіх цільових запитів. Формуємо технічне завдання.",
+			"ru":"После мы обсуждаем проект и находим подходящее решение, продумывавем идею ресурса. После того, как поставлены задачи, определяется целевая аудитория. Создаём структуру сайта для отражения всех целевых запросов. Формируем техническое задание.",
+		},
+		"ah-2":{
+			"en":"Prototyping",
+			"ua":"Створення прототипу",
+			"ru":"Создание прототипа",
+		},
+		"at-2":{
+			"en":"We are preparing a prototype of the main page for desktop, mobile devices, tablets, taking into account user experience and maximum ease of use. We coordinate all working moments.",
+			"ua":"Ми готуємо прототип головної сторінки для десктопу, мобільних пристроїв, планшетів з урахуванням досвіду користувача і максимальної зручності використання. Узгоджуємо усі робочі моменти.",
+			"ru":"Мы готовим прототип главной страницы для десктопа, мобильных устройств, планшетов с учётом пользовательского опыта и максимального удобства использования. Согласовываем все рабочие моменты.",
+		},
+		"ah-3":{
+			"en":"Design development",
+			"ua":"Розробка дизайну",
+			"ru":"Разработка дизайна",
+		},
+		"at-3":{
+			"en":"We create resource design taking into account the brand book. If not, it is possible to develop a corporate identity from scratch. We coordinate the whole design with you!",
+			"ua":"Ми створюємо дизайн ресурсу з урахуванням брендбуку. Якщо його немає, можлива розробка фірмового стилю з нуля. Узгоджуємо з Вами весь дизайн!",
+			"ru":"Мы создаём  дизайн ресурса с учётом брендбука. Если его нет, возможна разработка фирменного стиля с нуля. Согласовываем с Вами весь дизайн! ",
+		},
+		"ah-4":{
+			"en":"Layout and programming",
+			"ua":"Верстка та програмування",
+			"ru":"Вёрстка и программирование",
+		},
+		"at-4":{
+			"en":"We implement cross-browser layout and page programming. We carry out CMS integration, if necessary. We are making final changes to the site.",
+			"ua":"Ми реалізуємо кросербраузерну верстку та програмування сторінок. Проводимо інтеграцію CMS, якщо це потрібно. Вносимо останні редагування по сайту.",
+			"ru":"Мы реализуем кроссербраузерную вёрстку и программирование страниц. Проводим интеграцию CMS, если это необходимо. Вносим последние правки по сайту.",
+		},
+		"ah-5":{
+			"en":"Launching your site!",
+			"ua":"Запуск сайту!",
+			"ru":"Запуск Вашего сайта!",
+		},
+		"at-5":{
+			"en":"We upload the site to the hosting, configure and optimize. The most important role in the process of creating a site is its testing. The tester performs the work, which monitors the functionality of the resource according to a number of criteria and identifies errors that need to be fixed.",
+			"ua":"Ми завантажуємо сайт на хостинг, проводимо налаштування та оптимізацію. Найважливішу роль у процесі створення сайту відіграє його тестинг. Виконує роботу тестувальник, який моніторить функціональність ресурсу за низкою критеріїв та виявляє помилки, які мають бути усунені.",
+			"ru":"Мы загружаем сайт на хостинг, проводим настройку и оптимизацию. Важнейшую роль в процессе создания сайта играет его тестинг. Выполняет работу тестировщик, который мониторит функциональность ресурса по ряду критериев и выявляет ошибки, которые должны быть устранены.",
+		},
+		"ah-6":{
+			"en":"Project handover",
+			"ua":"Здача проекту",
+			"ru":"Сдача проекта",
+		},
+		"at-6":{
+			"en":"We hand over the project and all documentation on it. You get a ready-made resource with all access to it, change the password in the admin panel.<br> From now on, you are the only owner of the site!",
+			"ua":"Ми здаємо проект і всю документацію щодо нього. Ви отримуєте готовий ресурс з усіма доступами до нього, міняєте пароль в адмінці. З цього моменту Ви єдиний власник сайту!",
+			"ru":"Мы сдаём проект и всю документацию по нему. Вы получаете готовый ресурс со всеми доступами к нему, меняете пароль в админке.<br> С этого момента Вы единственный владелец сайта!",
+		},
+		"fbp-1":{
+			"en":"For more information<br> write to us and we will contact you<br> to answer your questions.",
+			"ua":"Для отримання більш детальної інформації<br> напишіть нам і ми зв'яжемося з Вами,<br> щоб відповісти на Ваші запитання.",
+			"ru":"Для получения более детальной информации<br> напишите нам и мы свяжемся с Вами,<br> чтобы ответить на Ваши вопросы.",
+		},
+		"fbp-2":{
+			"en":"* - mandatory fields",
+			"ua":"* - поля обов'язкові до заповнення",
+			"ru":"* - поля обязательные к заполнению",
+		},
+		"bs":{
+			"en":"Send",
+			"ua":"Відправити",
+			"ru":"Отправить",
+		},
+		"telephon-f":{
+			"en":"Telephone",
+			"ua":"Телефон",
+			"ru":"Телефон",
+		},
+		"socicons-f":{
+			"en":"We are on social networks",
+			"ua":"Ми у соцмережах",
+			"ru":"Мы в соцсетях",
+		},
+		"footer-1":{
+			"en":"© 2020-2023 All rights reserved. Made with ",
+			"ua":"© 2020-2023 Всі права захищені. Зроблено з ",
+			"ru":"© 2020-2023  Все права защищены. Сделано с ",
+		},
+		"footer-2":{
+			"en":" and ",
+			"ua":" та ",
+			"ru":" и ",
+		},
+		"popup":{
+			"en":"Leave your details<br> and we will call you back",
+			"ua":"Залишіть Ваші дані і ми Вам передзвонимо",
+			"ru":"Оставьте Ваши данные<br>и мы Вам перезвоним",
+		},
+		"popup-bs":{
+			"en":"Send",
+			"ua":"Відправити",
+			"ru":"Отправить",
+		},
+
+
+
+
+
+}
